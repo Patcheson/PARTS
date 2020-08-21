f01654fdc9abf3cbbdf8674c1e33baf169215b5e
Patcheson <rawkitofficialza@gmail.com>
2020-08-21 12:04:27 +0200

    guess.py

---
+++ pmid.py
@@ -0,0 +1,14 @@
+height = int(input("enter size"))
+
+for str_put in range(0,height):
+    for str_ptt in range(0,height-str_put-1):
+        print(" ")
+
+    for str_ptt in range(0,str_put+1):
+            
+       print("*")
+       print("\n")
+
+    #for str_ptt in range ( 0, height+ 1):# print("\n")
+      #  print("*")
+       # print(" ")j
